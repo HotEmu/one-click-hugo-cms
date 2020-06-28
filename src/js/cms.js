21d19679c43e441aafdc572740a6182a4390e734
Robert Haidari <roberthaidari@Roberts-MacBook-Pro.local>
2020-06-28 13:47:25 -0400

    CMS Setup

--- src/js/cms.js
+++ src/js/cms.js
@@ -10,10 +10,41 @@ import ProductsPreview from "./cms-preview-templates/products";
 import ValuesPreview from "./cms-preview-templates/values";
 import ContactPreview from "./cms-preview-templates/contact";
 
-CMS.registerPreviewStyle(styles, { raw: true });
+CMS.registerPreviewStyle(styles, {raw: true});
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
+CMS.registerEditorComponent({
+  id: "gist",
+  label: "Gist",
+  fields: [{
+    name: "username",
+    label: "Github Username",
+    widget: "string"
+  },
+  {
+    name: "gid",
+    label: "Gist ID",
+    widget: "string"
+  },
+  ],
+  pattern: /{{< gist ([a-zA-Z0-9]+) ([a-zA-Z0-9]+) >}}/,
+  fromBlock: function(match) {
+    return {
+      username: match[1],
+      gid: match[2],
+    };
+  },
+  toBlock: function(obj) {
+    return `{{< gist ${obj.username} ${obj.gid} >}}`;
+  },
+  toPreview: function(obj) {
+    return `{{< gist ${obj.username} ${obj.gid} >}}`;
+  },
+});
+
 CMS.init();
+
+
